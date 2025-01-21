abd87d1dfb3d6ffec36b34268280e9e24823043e
Ashlynn Hall <hall.ash9@gmail.com>
2025-01-21 14:36:20 -0700

    dynamic cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,14 +1,18 @@
-import { setLocalStorage, getLocalStorage } from './utils.mjs';
+import { setLocalStorage, getLocalStorage, getParam } from './utils.mjs';
 import { findProductById } from './productData.mjs';
+import { productDetails } from './productDetails.mjs';
 
-function addProductToCart(product) {
-  let cartItems = getLocalStorage('so-cart');
-  if (!cartItems || !Array.isArray(cartItems)) {
-    cartItems = [];
-  }
-  cartItems.push(product);
-  setLocalStorage('so-cart', cartItems);
-}
+// Moved it to productDetails.mjs
+// function addProductToCart(product) {
+//   let cartItems = getLocalStorage('so-cart');
+//   if (!cartItems || !Array.isArray(cartItems)) {
+//     cartItems = [];
+//   }
+//   cartItems.push(product);
+//   setLocalStorage('so-cart', cartItems);
+// }
+
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
@@ -19,3 +23,8 @@ async function addToCartHandler(e) {
 document
   .getElementById('addToCart')
   .addEventListener('click', addToCartHandler);
+
+const productId = getParam('product');
+productDetails(productId, '.product-detail');
+
+console.log(findProductById(productId));
