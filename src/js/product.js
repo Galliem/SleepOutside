103a760ebaf4a99d21db9c8a2a1981dbdb0e8a05
galliem <gar22094@byui.edu>
2025-01-31 10:54:05 -0700

    fixed first trello card

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
@@ -6,8 +6,18 @@ function addProductToCart(product) {
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
+  //check if there is something in the list, pull from localstorage
+    let cartItems = getLocalStorage("so-cart");
   const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  //check if list is empty
+  if (!cartItems) {
+    cartItems = []
+  }
+//add new product to the list
+cartItems.push(product)
+//add list to cart
+  
+  addProductToCart(cartItems);
 }
 
 // add listener to Add to Cart button
