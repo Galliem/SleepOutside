8676985707eee86a82b128b92247b8c2e8316404
jmchargue54 <jmchargue54@gmail.com>
2025-02-25 10:32:34 -0700

    beginning steps
    Co-authored-by: sadiebybee <sadiebybee@users.noreply.github.com>
    Co-authored-by: Galliem <Galliem@users.noreply.github.com>

--- src/js/cart.js
+++ src/js/cart.js
@@ -25,6 +25,7 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <a href="../checkout/index.html">Checkout</a>
 </li>`;
 
   return newItem;
---
+++ src/js/components/CheckoutForm.svelte
@@ -0,0 +1,9 @@
+<script>
+    let tax = 0;
+</script>
+
+<fieldset class="checkout-summary">
+    <legend>Order Summary</legend>
+
+    <p>${tax}</p>
+</fieldset>
